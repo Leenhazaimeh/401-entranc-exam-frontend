d4ff08fe2357c8eed040472d21a8c35f552f641d
Leen Hazaimeh <leenhazaimeh94@gmail.com>
2021-07-06 14:19:01 +0300

    done

---
+++ client/src/App.js
@@ -0,0 +1,33 @@
+
+  import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+import Header from "./components/Header" ;
+import Main from "./components/Main";
+import Favorite from "./components/Favorite";
+
+class App extends React.Component {
+
+  render() {
+    return(
+      <>
+        <Router>
+          <Header/>
+          <Switch>
+            <Route exact path="/">
+              <Main/>
+            </Route>
+            <Route exact path="/favorite">
+              <Favorite/>
+            </Route>
+          </Switch>
+        </Router>
+      </>
+    )
+  }
+}
+
+export default App;
